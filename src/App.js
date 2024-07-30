2e26f62ae87a049aac237ae9d739e87a996fcb1d
ridhikalsi3 <147323282+ridhikalsi3@users.noreply.github.com>
2024-07-30 17:09:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginSignUp from './components/LoginSignUp/LoginSignUp';
+//import  Login  from './components/LoginSignUp/Login';
+//import Sign from './components/LoginSignUp/Sign';
+
+function App() {
+  return (
+    <div>
+      <LoginSignUp/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/LoginSignUp/LoginSignUp.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './LoginSignUp.css'
+
+
+export const LoginSignUp = () => {
+  return (
+    <div className='sub-container'>
+        <div className="sub">Login</div>
+        <br />
+        <div className="sub">Sign Up</div>
+    </div>
+  )
+}
+
+export default LoginSignUp
